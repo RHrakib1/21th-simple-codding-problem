88a9cbe2b98aca56c51db9065afaa18a8b6b6393
RHrakib1 <rakibhasanmd457@gmail.com>
2024-02-06 10:54:10 +0600

    math

---
+++ math.js
@@ -0,0 +1,43 @@
+// onek number theke sorbo nimno number ber korar niyom 
+const min = Math.min(125, 20, 40, 30, 45, 880, 142, 146, 210);
+console.log("the min number is :", min);
+
+// onek number theke sorbocho number ber korar niyom
+const max = Math.max(12, 11, 21, 154, 521, 124, 810, 620,);
+console.log("the max number is :", max);
+
+
+// pai er man ber korrar jonno
+console.log('exapmel for the pai er man ber kora');
+console.log(Math.PI);
+
+//math.ads er madhome amra 2ta number er modhe gep koto ase seta bujha jai mane (5-10)=-5 hoi kintu 'ads' use krle sudhu 5 dibe r amni savabik vabe (10-5)=5 e hoi 
+console.log('exapmel for the 2ta number modhe gep koto ase ta dekhai');
+console.log(Math.abs(5 - 15));
+
+
+
+// kono vognaonsho k purno number korar jonnno "Math.round" use krte hoi mane nikotborti purno number kore ;
+console.log('exapmel for the vognaonsho To purno number');
+console.log(Math.round(5.72));
+
+
+
+// "Math.floor" use krle amra dosomik er pore ja thakbe ta bad diye jeta thakbe ta hbe
+console.log("exaple to the floor");
+console.log(Math.floor(6.55));
+
+
+
+// ceil hosse floor er biporit akhane dosomik er por kono num thakle purotai purno numb er mane "7.20" hole "8" hbe
+console.log("example to the ceil");
+console.log(Math.ceil(7.20));
+
+
+
+// khali random dile dosomik er age kono number thakbe na pore hbe akekbae=r akek ta hbe
+console.log("example")
+console.log(Math.random());
+// dosomik er age pore number use krle amrra akekbar akek man pabo ja diye gun dibo setar modhei asbe 
+console.log("example gun")
+console.log(Math.random()*5);
